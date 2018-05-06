2b906fc2db9ac2afc326fc5dbee5e0a8a014b1f0
dnikhil135 <dnikhil11@gmail.com>
2018-05-06 10:17:37 +0530

    implement Rabbit MQ

--- src/main/java/com/nik/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/nik/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,13 +1,11 @@
- package com.nik.microservices.currencyconversionservice;
+package com.nik.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.nik.microservices.currencyconversionservice.feign")
-@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
